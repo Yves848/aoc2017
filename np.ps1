6f176a1be4d37787cdf8762eb321f418d6fc3759
yves848 <yves.godart@gmail.com>
2025-02-13 12:19:09 +0100

    01-1

--- np.ps1
+++ np.ps1
@@ -2,11 +2,24 @@ param (
   [string]$project
 )
 
+$session = Get-Content -Path "./session.2017"
+$domain = ".adventofcode.com"
+$p = [int]$project
+
+
 mcd $project
 dotnet new console
 dotnet new sln
 dotnet sln add $project".csproj"
-cp ../Program.cs .
-ni test.txt
-ni data.txt
-cd ..
+Copy-Item ../Program.cs .
+New-Item test.txt
+New-Item data.txt
+$websession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$cookie = New-Object System.Net.Cookie
+$cookie.Name = "session"
+$cookie.Value = $session
+$cookie.Domain = $domain
+$websession.Cookies.Add($cookie)
+$uri = "https://adventofcode.com/2017/day/$($p)/input" 
+Invoke-WebRequest -Uri $uri -WebSession $websession -OutFile ./data.txt
+Set-Location ..
