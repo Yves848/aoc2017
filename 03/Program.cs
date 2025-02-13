cca9b2be41c911d06e1f0a1f3e7cd803ce1ae151
yves848 <yves.godart@gmail.com>
2025-02-13 16:58:37 +0100

    03

---
+++ 03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string puzzle = "277678";
+if (args.Length > 0) {
+  puzzle = args[0];
+}
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+
+
+void part1()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
