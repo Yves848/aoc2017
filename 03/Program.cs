86a9f95ee34fa889fec0db5f241c9eea7d29d19c
yves848 <yves.godart@gmail.com>
2025-02-14 16:57:23 +0100

    03

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,17 +1,55 @@
 ﻿using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
 
+
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-string puzzle = "277678";
-if (args.Length > 0) {
-  puzzle = args[0];
+int puzzle = 277678;
+if (args.Length > 0)
+{
+  puzzle = int.Parse(args[0]);
 }
 var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
 
+(int x, int y)[] dirs = {
+  (1,0),   // right
+  (0,-1),  // up
+  (-1,0),  // left
+  (0,1)   // down
+};
+
+Dictionary<(int x, int y), int> spiral = [];
 
 void part1()
 {
   int ans = 0;
+  int i = 0;
+  int d = 0;
+  int inc = 1;
+  // spiral.Add((0, 0), 1);
+  (int x, int y) = (0, 0);
+  while (i < puzzle)
+  {
+    
+    for (int step = 0; step < 2; step++)
+    {
+      (int dx, int dy) = dirs[d];
+      for (int j = 0; j < inc; j++)
+      {
+        spiral.Add((x, y), i + 1);
+        x += dx;
+        y += dy;
+        i++;
+      }
+      d++;
+      if (d > dirs.Count() - 1)
+      {
+        d = 0;
+      }
+    }
+    inc++;
+  }
+  Console.WriteLine(spiral.Last().Key);
+  ans = Math.Abs(0 - spiral.Last().Key.x) + Math.Abs(0 - spiral.Last().Key.y);
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
