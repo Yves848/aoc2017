472688765cc4bf24f69c8fcfbee0719281854c97
yves848 <yves.goadrt@gmail.com>
2025-02-15 17:50:52 +0100

    03-02

--- 03/Program.cs
+++ 03/Program.cs
@@ -14,7 +14,11 @@ var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
   (1,0),   // right
   (0,-1),  // up
   (-1,0),  // left
-  (0,1)   // down
+  (0,1),   // down
+  (1,-1),  // Up-Right
+  (-1,-1), // Up-Left
+  (1,1),   // Down-Right
+  (-1,1)   // Up-Left
 };
 
 Dictionary<(int x, int y), int> spiral = [];
@@ -48,7 +52,7 @@ void part1()
         i++;
       }
       d++;
-      if (d > dirs.Count() - 1)
+      if (d > 3)
       {
         d = 0;
       }
@@ -62,8 +66,14 @@ void part1()
 }
 
 int sum((int x, int y) cell) {
-  int result = 1;
-
+  int result = 0;
+  dirs.ToList().ForEach(d => {
+    var (dx,dy) = d;
+    if (spiral.ContainsKey((cell.x+dx,cell.y+dy))) {
+      result += spiral[(cell.x+dx,cell.y+dy)];
+    }
+  });
+  if (result == 0) result = 1;
   return result;
 }
 
@@ -73,25 +83,31 @@ void part2()
   int i = 0;
   int d = 0;
   int inc = 1;
-  spiral.Add((0, 0), 1);
+  //spiral.Add((0, 0), 1);
+  spiral.Clear();
   (int x, int y) = (0, 0);
-  while (i < puzzle - 1)
+  while (ans < puzzle)
   {
     for (int step = 0; step < 2; step++)
     {
       (int dx, int dy) = dirs[d];
       for (int j = 0; j < inc; j++)
       {
-        if (i < puzzle)
+        if (ans == 0)
         {
-          spiral.Add((x, y), i + 1);
+          int s = sum((x,y));
+          spiral.Add((x, y), s);
+          if (s > puzzle) {
+            ans = s;
+            break;
+          }
           x += dx;
           y += dy;
         }
         i++;
       }
       d++;
-      if (d > dirs.Count() - 1)
+      if (d > 3)
       {
         d = 0;
       }
