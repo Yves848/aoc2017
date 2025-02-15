002706a8776d967cbac91ecb72fad917862c20a0
yves848 <yves.goadrt@gmail.com>
2025-02-15 17:06:17 +0100

    01-01

--- 03/Program.cs
+++ 03/Program.cs
@@ -19,6 +19,7 @@ var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
 
 Dictionary<(int x, int y), int> spiral = [];
 
+(int x, int y) center = (Console.BufferWidth / 2, Console.BufferHeight / 2);
 void part1()
 {
   int ans = 0;
@@ -27,17 +28,23 @@ void part1()
   int inc = 1;
   // spiral.Add((0, 0), 1);
   (int x, int y) = (0, 0);
-  while (i < puzzle)
+  while (i < puzzle - 1)
   {
-    
+
     for (int step = 0; step < 2; step++)
     {
       (int dx, int dy) = dirs[d];
       for (int j = 0; j < inc; j++)
       {
-        spiral.Add((x, y), i + 1);
-        x += dx;
-        y += dy;
+        if (i < puzzle)
+        {
+          spiral.Add((x, y), i + 1);
+          // Console.CursorLeft = center.x + x;
+          // Console.CursorTop = center.y + y;
+          // Console.Write(i + 1);
+          x += dx;
+          y += dy;
+        }
         i++;
       }
       d++;
@@ -48,14 +55,49 @@ void part1()
     }
     inc++;
   }
+  Console.CursorTop = Console.BufferHeight - 2;
   Console.WriteLine(spiral.Last().Key);
   ans = Math.Abs(0 - spiral.Last().Key.x) + Math.Abs(0 - spiral.Last().Key.y);
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
+int sum((int x, int y) cell) {
+  int result = 1;
+
+  return result;
+}
+
 void part2()
 {
   int ans = 0;
+  int i = 0;
+  int d = 0;
+  int inc = 1;
+  spiral.Add((0, 0), 1);
+  (int x, int y) = (0, 0);
+  while (i < puzzle - 1)
+  {
+    for (int step = 0; step < 2; step++)
+    {
+      (int dx, int dy) = dirs[d];
+      for (int j = 0; j < inc; j++)
+      {
+        if (i < puzzle)
+        {
+          spiral.Add((x, y), i + 1);
+          x += dx;
+          y += dy;
+        }
+        i++;
+      }
+      d++;
+      if (d > dirs.Count() - 1)
+      {
+        d = 0;
+      }
+    }
+    inc++;
+  }
   Console.WriteLine($"Part 2 - Answer : {ans}");
 }
 
