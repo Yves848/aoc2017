199dc6e57d0ad927696512a3794b7edc63a804d1
yves848 <yves.goadrt@gmail.com>
2025-02-16 10:55:23 +0100

    06 WIP

--- 04/Program.cs
+++ 04/Program.cs
@@ -2,19 +2,52 @@
 using System.Runtime.InteropServices;
 
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2017/01/test.txt").ToList();
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2017/04/test.txt").ToList();
 var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
 
 
 void part1()
 {
   int ans = 0;
+  int i = 0;
+  while(i < file.Count) {
+    var mots = file[i].Split(" ").ToList();
+    int j = 0;
+    bool valide = true;
+    while (j < mots.Count && valide) {
+      valide = valide && mots.Where(m => m == mots[j]).Count() == 1;
+      j++;
+    }
+    if (valide) ans++;
+    i++;
+  }
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
 void part2()
 {
   int ans = 0;
+  int i = 0;
+  while(i < file.Count) {
+    var mots = file[i].Split(" ").ToList();
+    int j = 0;
+    bool valide = true;
+    while (j < mots.Count && valide) {
+      List<char> mot = mots[j].ToCharArray().ToList();
+      mot.Sort();
+      string m1 = String.Join("",mot);
+      int count = 0;
+      mots.Where(m => m.Length == mot.Count).ToList().ForEach(cible => {
+        List<char> mot2 = cible.ToCharArray().ToList();
+        mot2.Sort();
+        if (m1 == String.Join("",mot2)) count++;
+      });
+      valide = valide && count < 2;
+      j++;
+    }
+    if (valide) ans++;
+    i++;
+  }
   Console.WriteLine($"Part 2 - Answer : {ans}");
 }
 
---
+++ 05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2017/05/test.txt").ToList();
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+List<int> stack = file.Select(i => int.Parse(i)).ToList();
+
+void part1()
+{
+  int ans = 0;
+  int i = 0;
+  while (i >= 0 && i < stack.Count) {
+    var next = i + stack[i];
+    stack[i] = stack[i] +1;
+    i = next;
+    ans++;
+  }
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  stack = file.Select(i => int.Parse(i)).ToList();
+  int ans = 0;
+  int i = 0;
+  while (i >= 0 && i < stack.Count) {
+    var next = i + stack[i];
+    stack[i] = stack[i] >= 3 ? stack[i] -1 : stack[i] +1;
+    i = next;
+    ans++;
+  }
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
